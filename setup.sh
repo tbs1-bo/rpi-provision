7ef630ffa5017bb159933e14f7eb8ed8662184f6
Marco Bakera <marco@bakera.de>
2018-06-15 15:37:22 +0200

    added info.

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ echo Provisioning Pi
 echo 
 echo new hostname: $NEWHOSTNAME
 echo new password for user pi: $NEWPASSWORD
+echo Remember or write down the information above!
 echo Pi will be rebooted at the end.
 echo Press Enter to start or CTRL-C to abort.
 read
